cff7a68a36ffdc6bde6c6792357cf4ba323b1bca
Cyrille Morin <cyrille.morin@inria.fr>
2023-03-08 18:49:45 +0100

    Help with execution inside container
    Signed-off-by: Cyrille Morin <cyrille.morin@inria.fr>

--- examples/createFolderAndFiles_usrp.py
+++ examples/createFolderAndFiles_usrp.py
@@ -45,14 +45,14 @@ for rx_idx, rx in enumerate(rx_nodes):
 			yaml_content += f"  node{node_id}:\n"
 			yaml_content +=  "    container:\n"
 			yaml_content += f"    - image: {docker_img_string}\n"
-			yaml_content += f"      command: reciever.py -R re_{rx_idx:02d} -I im_{rx_idx:02d} -T {len(tx_nodes)}\n\n"  # TODO Add num samples to save?
+			yaml_content += f"      command: bash -lc \"/root/gr-txid/examples/src/reciever.py -R re_{rx_idx:02d} -I im_{rx_idx:02d} -T {len(tx_nodes)}\"\n\n"  # TODO Add num samples to save?
 			continue
 
 		if node_id == args.sched_node: # TODO Allow scheduler to run on a Tx or Rx node?
 			yaml_content += f"  node{node_id}:\n"
 			yaml_content +=  "    container:\n"
 			yaml_content += f"    - image: {docker_img_string}\n"
-			yaml_content += f"      command: scheduler.py -p {args.port_num} -s {args.tx_freq} -n {tx_node_string}\n"
+			yaml_content += f"      command: bash -lc \"/root/gr-txid/examples/src/scheduler.py -p {args.port_num} -s {args.tx_freq} -n {tx_node_string}\"\n"
 			yaml_content +=  "    passive: true\n\n"
 			continue
 		
@@ -60,7 +60,7 @@ for rx_idx, rx in enumerate(rx_nodes):
 			yaml_content += f"  node{node_id}:\n"
 			yaml_content +=  "    container:\n"
 			yaml_content += f"    - image: {docker_img_string}\n"
-			yaml_content += f"      command: emitter.py -P {args.port_num} -T {tx_nodes.index(node_id)} -G {default_gain} -R {int(args.is_random)} -r {int(args.is_noise)} -f {pow_var_speed * int(args.is_var)} -M 0.5\n"
+			yaml_content += f"      command: bash -lc \"/root/gr-txid/examples/src/emitter.py -P {args.port_num} -T {tx_nodes.index(node_id)} -G {default_gain} -R {int(args.is_random)} -r {int(args.is_noise)} -f {pow_var_speed * int(args.is_var)} -M 0.5\"\n"
 			yaml_content +=  "    passive: true\n\n"
 	
 	# Folder creation
